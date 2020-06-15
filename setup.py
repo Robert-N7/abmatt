98e7af6f5c8f15c6585f567e9d9f8147555c50e5
Robert Nelson <nelsrob2@isu.edu>
2020-06-15 15:24:21 -0600

    version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ CLASSIFIERS = [
 
 # calling the setup function
 setup(name='abmatt',
-      version='0.2.1',
+      version='0.4.0',
       entry_points={
           'console_scripts': [
               'abmatt = abmatt.__main__:main'
