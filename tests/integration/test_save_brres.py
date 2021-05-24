261e31cb2285f0c52c7cb52872290b6a6ae76d52
Robert Nelson <32576389+Robert-N7@users.noreply.github.com>
2021-05-23 19:04:13 -0600

    Added --moonview option

--- tests/integration/test_save_brres.py
+++ tests/integration/test_save_brres.py
@@ -5,7 +5,7 @@ from tests.lib import AbmattTest
 
 class TestSaveBrres(IntegTest, AbmattTest):
     def test_save_moonview(self):
-        self.assertTrue(self._abmatt('-b brres_files/crazy_cannon.brres -d test_files/tmp.brres -o --moonview'))
+        self.assertTrue(self._abmatt('-b brres_files/simple.brres -d test_files/tmp.brres -o --moonview'))
         b = Brres(self._get_tmp('.brres', False))
         self.assertFalse(b.check_moonview())
         self.assertFalse(b.MOONVIEW)
